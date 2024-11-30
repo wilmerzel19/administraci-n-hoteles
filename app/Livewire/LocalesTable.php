45342f2fffab30ffe15f45c473ba7031e88e423b
wilmer <est.wzelayam@uml.edu.ni>
2024-11-30 12:18:46 -0600

    creacion de crear hotel y formularios guardar

--- app/Livewire/LocalesTable.php
+++ app/Livewire/LocalesTable.php
@@ -18,25 +18,34 @@ class LocalesTable extends DataTableComponent
     public function columns(): array
     {
         return [
-            Column::make("Id", "id")
-                ->sortable(),
+          //  Column::make("Id", "id")
+           //     ->sortable(),
             Column::make("Nombre", "nombre")
                 ->sortable(),
             Column::make("Imagen", "imagen")
-                ->sortable(),
-            Column::make("Direccion", "direccion")
-                ->sortable(),
+                ->sortable()
+                ->format(function ($value, $column, $row) {
+                    return '<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.freepik.com%2Fvector-gratis%2Filustracion-concepto-torre-hotel_114360-12962.jpg&tbnid=rhhZaTxxHj7UPM&vet=10CAgQxiAoB2oXChMI0OXY6sGEigMVAAAAAB0AAAAAEAc..i&imgrefurl=https%3A%2F%2Fwww.freepik.es%2Fvectores%2Fhotel%2F12&docid=Dv6xBBb3R2LvbM&w=626&h=626&itg=1&q=hoteles%20png&ved=0CAgQxiAoB2oXChMI0OXY6sGEigMVAAAAAB0AAAAAEAc' . $value . '" width="100" height="100" />';
+                })
+                ->html(),
+
+           // Column::make("Direccion", "direccion")
+           //     ->sortable(),
             Column::make("Descripcion", "descripcion")
                 ->sortable(),
             Column::make("Telefono", "telefono")
                 ->sortable(),
-            Column::make("Website", "website")
-                ->sortable(),
-            Column::make("Calificacion", "calificacion")
-                ->sortable(),
+            //Column::make("Website", "website")
+               // ->sortable(),
+           // Column::make("Calificacion", "calificacion")
+           //     ->sortable(),
             Column::make("Tipo", "tipo")
                 ->sortable(),
-          
+                Column::make('id')
+    ->format(
+        fn($value, $row, Column $column) => view('livewire.acciones.locales.acciones')->with('value', $value)
+    ),
+
         ];
     }
 }
---
+++ resources/views/livewire/acciones/locales/acciones.blade.php
@@ -0,0 +1,3 @@
+<x-primary-button class="mt-4" type="submit">Editar</x-primary-button>
+
+<x-danger-button class="mt-4" wire:click="eliminar">Eliminar</x-danger-button>
